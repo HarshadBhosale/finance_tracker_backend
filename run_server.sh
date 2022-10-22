99455d54878e84f09bcb3d87b484c723d8ccd47b
HarshadBhosale <harshad.bhosale@cogoport.com>
2022-10-22 18:43:10 +0530

    run server updated

--- run_server.sh
+++ run_server.sh
@@ -1,1 +1,8 @@
+export DATABASE_NAME="db_name"
+export DATABASE_USER_NAME="db_user"
+export DATABASE_PASSWORD="db_pwd"
+export DATABASE_HOST="localhost or db_host"
+export DATABASE_PORT="5432 or db_port"
+
 uvicorn main:finance_tracker_api --reload
+# uvicorn main:finance_tracker_api --host 0.0.0.0 --port 80 --reload
